7f3726e61637e0c5e65146f7aa04d3c0fb16ed6f
POPIVUS <909slender909@gmail.com>
2021-03-04 15:18:05 +0300

    Mark

---
+++ Mark1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mark1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
